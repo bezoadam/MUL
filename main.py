95d9dda674683c692af2a6f058f1e91422856750
Adam Bezak <bezoadam95@gmail.com>
2019-02-25 19:22:29 +0100

    Fix pycharm project, add testing QListWidgetItems

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from PyQt5 import QtCore, QtGui, QtWidgets #
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 import qtCreatorProject.MP3Player.mp3PlayerGUI
 
-class Mp3Player(QtWidgets.QMainWindow, qtCreatorProject.MP3Player.mp3PlayerGUI.Ui_MainWindow):
+class Mp3Player(QtWidgets.QMainWindow, qtCreatorProject.MP3Player.mp3PlayerGUI.Ui_mainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+
+    def add(self):
+        for i in range(10):
+            item = QtWidgets.QListWidgetItem("Item %i" % i)
+            self.listWidget.addItem(item)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     form = Mp3Player()
     form.show()
+    form.add()
     app.exec_()
-
 
 if __name__ == '__main__':
     main()
