58124fd44d44be3ad34a9100358569d4a0f647bc
Adam Bezak <bezoadam95@gmail.com>
2019-02-25 21:23:17 +0100

    Add volume to group, add signals

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import qtCreatorProject.MP3Player.mp3PlayerGUI
 
 class Mp3Player(QtWidgets.QMainWindow, qtCreatorProject.MP3Player.mp3PlayerGUI.Ui_mainWindow):
+    valueChanged = QtCore.pyqtSignal(int)
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.setupActions()
+        self.setupValueChanged()
 
     def setupActions(self):
         self.actionFile.triggered.connect(self.handleActionFile)
         self.actionSearch.triggered.connect(self.handleActionSearch)
+
+    def setupValueChanged(self):
+        self.volumeProgressBar.valueChanged.connect(self.handleProgressBarValue)
+        self.volumeDial.valueChanged.connect(self.volumeProgressBar.setValue)
+        self.volumeDial.valueChanged.connect(self.valueChanged)
 
     def handleActionFile(self):
         path = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
@@ -32,6 +39,13 @@ class Mp3Player(QtWidgets.QMainWindow, qtCreatorProject.MP3Player.mp3PlayerGUI.U
 
     def handleActionSearch(self):
         print("handle search")
+
+    def handleProgressBarValue(self, value):
+        print(value)
+
+    def setValue(self, value):
+        print(value)
+        self.volumeProgressBar.setValue(value)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
